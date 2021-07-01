75bc055ff04e107798a5044c4d22d89c909e9177
phuong2904 <86756656+phuong2904@users.noreply.github.com>
2021-07-01 11:59:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
     introTitle: 'Bae à!',
-    introDesc: `Trái đất vốn lạ thường
+    introDesc: ` trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim toyy lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ  thích <33',
+    btnYes: 'Vẫn cứ là thích <33',
     btnNo: 'Không, mơ à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao lại là toyy <3',
     btnReply: 'Gửi nuônnnn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'hí>< iluvutoo',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <3333',
+    mess: 'hí>< iluvutoo❤️',
     messDesc: 'la là la lá lợp❤️🐷.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
